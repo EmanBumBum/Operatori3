2c103a75014f32bca24d6aac094784f2fb4dfc44
Vale <vale93cic@hotmail..it>
2025-05-31 11:00:52 +0200

    esercizio completato

---
+++ main.js
@@ -0,0 +1,10 @@
+let num1, num2;
+num1=Number(prompt("scegli il primo numero"));
+num2=Number(prompt("scegli il secondo numero"));
+somma=num1+num2;
+sottrazione=num1-num2;
+moltiplicazione=num1*num2;
+divisione=num1/num2;
+potenza=num1**num2;
+
+console.log("Con i numeri da te scelti, i risultati delle varie operazioni sono: somma " + somma +", sottrazione " + sottrazione + " moltiplicazione " + moltiplicazione + " divisione " + divisione +" potenza " + potenza);
